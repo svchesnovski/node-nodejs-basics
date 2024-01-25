48b5c8097320732dc725b1842fa9f0a415cb14a2
svchesnovski <svchesnovski@gmail.com>
2024-01-25 16:53:40 +0300

    feat: add cli args function

--- src/cli/args.js
+++ src/cli/args.js
@@ -1,5 +1,12 @@
 const parseArgs = () => {
-    // Write your code here 
+    const args = process.argv.slice(2);
+    args.forEach((arrayElement, index) => {
+        if (index % 2 === 0) {
+            const propName = arrayElement.substring(2);
+            const value = args[index + 1];
+            console.log(`${propName} is ${value}`);
+        }
+    });
 };
 
 parseArgs();
