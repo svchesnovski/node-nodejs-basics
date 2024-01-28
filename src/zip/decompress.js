144d36619be640aa52028d536ed84380e0cea3b4
svchesnovski <svchesnovski@gmail.com>
2024-01-28 23:12:14 +0300

    feat: add decompress function

--- src/zip/decompress.js
+++ src/zip/decompress.js
@@ -1,5 +1,16 @@
+import fs from 'fs';
+import zlib from 'zlib';
+import { fileURLToPath } from 'url';
+import path from 'path';
+
 const decompress = async () => {
-    // Write your code here 
+    const __dirname = path.dirname(fileURLToPath(import.meta.url));
+    const currDir = path.join(__dirname, 'files')
+    const filePath = path.join(__dirname, 'files', 'archive.gz');
+    const readStream = fs.createReadStream(filePath);
+    const writeStream = fs.createWriteStream(path.join(currDir, 'fileToCompress.txt'));
+    const gunzip = zlib.createGunzip();
+    readStream.pipe(gunzip).pipe(writeStream);
 };
 
 await decompress();
