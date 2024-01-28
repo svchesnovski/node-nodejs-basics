94f41d9e29d5cbe72ca7dcf6ffacf5e634eeca94
svchesnovski <svchesnovski@gmail.com>
2024-01-28 20:07:23 +0300

    feat: add write function using writable stream

--- src/streams/write.js
+++ src/streams/write.js
@@ -1,5 +1,12 @@
+import fs from 'fs';
+import { fileURLToPath } from 'url';
+import path from 'path';
+
 const write = async () => {
-    // Write your code here 
+    const __dirname = path.dirname(fileURLToPath(import.meta.url));
+    const filePath = path.join(__dirname, 'files', 'fileToWrite.txt');
+    const createStream = fs.createWriteStream(filePath);
+    process.stdin.pipe(createStream);
 };
 
 await write();
