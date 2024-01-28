6d05dfb8087ccf0fa8186d1ebd402a9b7a04d3fb
svchesnovski <svchesnovski@gmail.com>
2024-01-28 17:31:12 +0300

    feat: add hash function

--- src/hash/calcHash.js
+++ src/hash/calcHash.js
@@ -1,5 +1,20 @@
+import crypto from 'crypto';
+import fs from 'fs';
+import { fileURLToPath } from 'url';
+import path from 'path';
+import { pipeline } from 'stream/promises';
+
 const calculateHash = async () => {
-    // Write your code here 
+    const __dirname = path.dirname(fileURLToPath(import.meta.url));
+    const filePath = path.join(__dirname, 'files', 'fileToCalculateHashFor.txt');
+    const createHash = crypto.createHash('sha256');
+    await pipeline(
+        fs.createReadStream(filePath),
+        createHash,
+        async () => {
+        console.log(createHash.digest('hex'));
+        }
+    );
 };
 
 await calculateHash();
